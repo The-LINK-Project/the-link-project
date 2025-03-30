ae954df620239fe5c937f4bcd699dd013af9919a
AlexLee09 <alexandrekmlee@gmail.com>
2025-03-30 10:16:09 +0800

    Simple side menu dropdown

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,11 +1,31 @@
+"use client"
 import React from 'react'
 import NavItems from './NavItems'
+import { MenuIcon } from 'lucide-react'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger, DropdownMenuItem } from '../ui/dropdown-menu'
+import { navLinks } from '../constants'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <div className='flex flex-row mx-24 bg-red-50 justify-between h-18 items-center'>
-            <h1 className=''>The LINK Project</h1>
-            <NavItems></NavItems>
+        <h1 className=''>The LINK Project</h1>
+        <NavItems className="hidden md:block" />
+
+    <div className='relative md:hidden'>
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <MenuIcon />
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                {navLinks.map((navLink) => (
+                    <DropdownMenuItem key={navLink.name} asChild>
+                        <Link href={navLink.route}>{navLink.name}</Link>
+                    </DropdownMenuItem>
+                ))}
+            </DropdownMenuContent>
+        </DropdownMenu>
+    </div>
     </div>
   )
 }
--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { navLinks } from '../constants'
 import Link from 'next/link'
+import { cn } from '@/lib/utils'
 
-const NavItems = () => {
+const NavItems = ({ className }: { className: string }) => {
   return (
-    <div className='space-x-10'>
+    <div className={cn('space-x-10', className)}>
         {navLinks.map((navLink) => (
             <Link href={navLink.route} key={navLink.name}>
                 {navLink.name}
