d739dee2dd91af571dffcad59778bc33603605aa
AlexLee09 <alexandrekmlee@gmail.com>
2025-05-17 11:27:53 +0800

    Basic 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 
 const NotFoundPage = () => {
   return (
-    <div>404 | No such page</div>
+    <div className='flex flex-col items-center pt-80 h-full gap-5'>
+      <div>404 | No such page</div>
+      <Button className='bg-main-blue hover:bg-main-blue-hover'>
+        <Link href={"/"}>Return</Link>
+      </Button>
+    </div>
   )
 }
 
