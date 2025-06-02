a19deed26dde2009af06fd3f46817e730a94cb37
AlexLee09 <alexandrekmlee@gmail.com>
2025-06-02 23:51:20 +0800

    Create new lesson progress action

---
+++ lib/actions/LessonProgress.actions.ts
@@ -0,0 +1,56 @@
+"use server"; 
+
+import { connectToDatabase } from "@/lib/database";
+import { revalidatePath } from "next/cache";
+import User from "@/lib/database/models/user.model";
+import LessonProgress from "../database/models/lessonProgress.model";
+import { auth } from "@clerk/nextjs/server";
+
+interface Message {
+    role: string;
+    message: string;
+  }
+export async function createLessonProgress(lessonProgress: {
+    lessonIndex: Number;
+    objectivesMet: Boolean[];
+    convoHistory: Message[]
+}) {
+    try {
+        await connectToDatabase();
+
+        const {sessionClaims} = await auth();
+
+        const userId = sessionClaims?.userId as string;
+
+        if (!userId) {
+            throw new Error ("User not found");
+        }
+
+        const payload = {
+            userId: userId,
+            lessonIndex: lessonProgress.lessonIndex,
+            objectivesMet: lessonProgress.objectivesMet,
+            convoHistory: lessonProgress.convoHistory
+        }
+
+        const newLessonProgress = await LessonProgress.create(payload);
+
+        if (!newLessonProgress)
+            throw Error ("Failed to create new lesson progress")
+
+        return JSON.parse(JSON.stringify(newLessonProgress));
+
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+
+
+// const LessonProgressSchema = new Schema({
+//     userId: {type: Schema.Types.ObjectId, ref: "User", required: true, unique: false },
+//     lessonIndex: {type: Number, required: true, unique: false },
+//     objectivesMet: {type: [Boolean], required: true, unique: false },
+//     convoHistory: {type: [{role: { type: String, required: true },message: { type: String, required: true }}],required: true, unique: false }
+// })
