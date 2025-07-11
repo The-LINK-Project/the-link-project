d239f937115c67b294794fe17e37161bd47c3882
AlexLee09 <alexandrekmlee@gmail.com>
2025-07-11 14:46:01 +0800

    Move code that doesn't require states to page.tsx quiz

--- app/(root)/learn/[lessonIndex]/quiz/page.tsx
+++ app/(root)/learn/[lessonIndex]/quiz/page.tsx
@@ -1,19 +1,35 @@
 import React from "react";
 import QuizClient from "@/components/quiz/QuizClient";
+import { getQuizByLessonId } from "@/lib/actions/quiz.actions";
 
 type QuizPageProps = {
   params: {
     lessonIndex: string;
   };
 };
-const page = ({ params }: QuizPageProps) => {
+const QuizPage = async ({ params }: QuizPageProps) => {
   const index = parseInt(params.lessonIndex, 10);
+  const quiz = await getQuizByLessonId(index);
 
   return (
-    <div>
-      <QuizClient params={{ lessonIndex: index }} />
+    <div className="min-h-screen flex flex-col items-center">
+      <div className="max-w-4xl w-full px-6 py-12">
+        <div className="text-center mb-10">
+          <div className="inline-flex items-center px-4 py-2 bg-[rgb(90,199,219)]/10 rounded-full text-[rgb(90,199,219)] text-sm font-medium mb-4">
+            Interactive Quiz
+          </div>
+          <h1 className="text-4xl font-bold text-[rgb(90,199,219)] mb-4">
+            {quiz.title}
+          </h1>
+          <p className="text-slate-600 text-lg max-w-2xl mx-auto">
+            Test your knowledge and track your progress
+          </p>
+        </div>
+      <QuizClient params={{ quiz: quiz }} />
+      </div>
     </div>
+
   );
 };
 
-export default page;
+export default QuizPage;
--- components/quiz/QuizClient.tsx
+++ components/quiz/QuizClient.tsx
@@ -1,40 +1,20 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { saveQuizResult } from "@/lib/actions/quizResults.actions";
-import { getQuizByLessonId } from "@/lib/actions/quiz.actions";
 import QuizComplete from "@/components/quiz/QuizComplete";
 import QuizIncomplete from "@/components/quiz/QuizIncomplete";
 
-type LessonPageProps = {
+type QuizClientProps = {
   params: {
-    lessonIndex: number;
+    quiz: QuizData;
   };
 };
 
-export default function QuizClient({ params }: LessonPageProps) {
-  const lessonIndex = params.lessonIndex;
-  const [quiz, setQuiz] = useState<QuizData>({
-    title: "",
-    lessonId: 0,
-    questions: [],
-  });
+export default function QuizClient({ params}: QuizClientProps) {
+  const quiz = params.quiz;
   const [selectedAnswers, setSelectedAnswers] = useState<number[]>(new Array(quiz.questions.length).fill(-1));
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [score, setScore] = useState<number | null>(null);
-
-
-  useEffect(() => {
-    const fetchQuiz = async () => {
-      try {
-        const data = await getQuizByLessonId(lessonIndex);
-        setQuiz(data);
-      } catch (error) {
-        console.error("Failed to fetch quiz:", error);
-      }
-    };
-
-    fetchQuiz();
-  }, []);
 
 
   const handleAnswerSelect = (questionIndex: number, answerIndex: number) => {
@@ -74,18 +54,6 @@ export default function QuizClient({ params }: LessonPageProps) {
   return (
     <div className="min-h-screen flex justify-center">
       <div className="max-w-4xl w-full px-6 py-12">
-        <div className="text-center mb-10">
-          <div className="inline-flex items-center px-4 py-2 bg-[rgb(90,199,219)]/10 rounded-full text-[rgb(90,199,219)] text-sm font-medium mb-4">
-            Interactive Quiz
-          </div>
-          <h1 className="text-4xl font-bold text-[rgb(90,199,219)] mb-4">
-            {quiz.title}
-          </h1>
-          <p className="text-slate-600 text-lg max-w-2xl mx-auto">
-            Test your knowledge and track your progress
-          </p>
-        </div>
-
         {!isSubmitted ? (
           <QuizIncomplete quiz={quiz} selectedAnswers={selectedAnswers} handleAnswerSelect={handleAnswerSelect} handleSubmit={handleSubmit} />
         ) : (
