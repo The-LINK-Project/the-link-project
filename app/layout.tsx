8c2f6a1ea609efb752f7994f2f2a6195289f4389
Adrish7 <adrishmajumder7@gmail.com>
2025-07-20 18:29:14 +0800

    Changed to green link icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
     siteName: "The LINK Project",
     images: [
       {
-        url: "/assets/link_blue.png",
+        url: "/assets/link_green.png",
         width: 600,
         height: 600,
         alt: "The LINK Project Logo",
@@ -66,7 +66,7 @@ export default function RootLayout({
       <html lang="en">
         <body
           className={`${openSans.variable} antialiased container mx-auto max-w-7xl`}
-          >
+        >
           <Chatbot></Chatbot>
           {children}
           <Analytics />
