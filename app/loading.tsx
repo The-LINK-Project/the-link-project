b58348d6dc054880a53f1690d92758a0b3778d66
RuhanGupta <gupta56411@gapps.uwcsea.edu.sg>
2025-07-25 02:00:32 +0800

    usedglobalcssvariables

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const LoadingScreen = () => {
   return (
-    <div className="h-screen flex flex-col items-center justify-center bg-white">
-      <div className="w-10 h-10 border-4 border-gray-300 border-t-gray-600 rounded-full animate-spin"></div>
-      <p className="mt-4 text-gray-700 text-lg">Loading...</p>
+    <div className="h-screen flex flex-col items-center justify-center bg-[--color-background]">
+      <div className="w-10 h-10 border-4 border-muted border-t-foreground rounded-full animate-spin" />
+      <p className="mt-4 text-foreground text-lg">Linking...</p>
     </div>
   );
 };
