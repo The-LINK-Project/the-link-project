65c6d3acf0a4371f92c30f5980f1568d50cef2e1
AlexLee09 <alexandrekmlee@gmail.com>
2025-07-26 16:51:49 +0800

    Fix sign in sign up off center

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,13 @@
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen">
-      <main className="">{children}</main>
-    </div>
-  );
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <div className="flex flex-col items-center justify-center w-full min-h-screen">
+            <main className="w-full flex items-center justify-center">
+                {children}
+            </main>
+        </div>
+    );
 }
