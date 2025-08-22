bc7e7e14a59291cfb0c1ac8dd4bcc2f4b4ffb3f2
AlexLee09 <alexandrekmlee@gmail.com>
2025-08-22 22:08:25 +0800

    photos

--- constants/index.ts
+++ constants/index.ts
@@ -1,73 +1,73 @@
 export const navLinks = [
-  {
-    name: "Home",
-    route: "/",
-  },
-  {
-    name: "Contact",
-    route: "/contact",
-  },
-  {
-    name: "About Us",
-    route: "/about",
-  },
-  {
-    name: "Get Started",
-    route: "/dashboard",
-  },
+    {
+        name: "Home",
+        route: "/",
+    },
+    {
+        name: "Contact",
+        route: "/contact",
+    },
+    {
+        name: "About Us",
+        route: "/about",
+    },
+    {
+        name: "Get Started",
+        route: "/dashboard",
+    },
 ];
 
 export const frequentlyAskedQuestions = [
-  {
-    question: "What is the LINK Project?",
-    answer:
-      "The LINK Project supports English learning for migrant workers in Singapore through a personalized AI voice assistant, helping improve daily communication and confidence.",
-  },
-  {
-    question: "When will the LINK Project launch",
-    answer: "The core features will be launched in July.",
-  },
-  {
-    question: "How can I contribute towards the LINK Project",
-    answer:
-      "You can share your ideas on the google form linked to the website to inform us of what you would like to see and any suggestions you have.",
-  },
-  {
-    question: "Will the LINK Project be free?",
-    answer:
-      "Yes, the LINK Project is completely free and no payment will be required.",
-  },
-  {
-    question: "What kind of support does the LINK Project provide?",
-    answer:
-      "The Link Project gives you a free AI voice assistant to help improve your English anytime, anywhere. It helps you learn words, speak clearly, and feel more confident talking to your boss or in daily life.",
-  },
+    {
+        question: "What is the LINK Project?",
+        answer:
+            "The LINK Project supports English learning for migrant workers in Singapore through a personalized AI voice assistant, helping improve daily communication and confidence.",
+    },
+    {
+        question: "When will the LINK Project launch",
+        answer: "The core features will be launched in July.",
+    },
+    {
+        question: "How can I contribute towards the LINK Project",
+        answer:
+            "You can share your ideas on the google form linked to the website to inform us of what you would like to see and any suggestions you have.",
+    },
+    {
+        question: "Will the LINK Project be free?",
+        answer:
+            "Yes, the LINK Project is completely free and no payment will be required.",
+    },
+    {
+        question: "What kind of support does the LINK Project provide?",
+        answer:
+            "The Link Project gives you a free AI voice assistant to help improve your English anytime, anywhere. It helps you learn words, speak clearly, and feel more confident talking to your boss or in daily life.",
+    },
 ];
 
 export const aboutUsInformation = [
-  {
-    name: "Adrish Majumder",
-    role: "Co-Founder",
-    image: "/assets/mun.jpeg",
-  },
-  {
-    name: "Alexandre Lee",
-    role: "Co-Founder",
-    image: "/assets/Alex.jgep",
-  },
-  {
-    name: "Ruhan Gupta",
-    role: "Co-Founder",
-    image: "/assets/Ruhan.jpeg",
-  },
-  {
-    name: "Videep Agarwal",
-    role: "Co-Founder",
-    image: "/assets/Videep.jpeg",
-  },
+    {
+        name: "Adrish Majumder",
+        role: "Co-Founder",
+        image: "/assets/about-us/mun.jpeg",
+    },
+    {
+        name: "Alexandre Lee",
+        role: "Co-Founder",
+        image: "/assets/about-us/Alex.jpeg",
+    },
+    {
+        name: "Ruhan Gupta",
+        role: "Co-Founder",
+        image: "/assets/about-us/Ruhan.jpeg",
+    },
+    {
+        name: "Videep Agarwal",
+        role: "Co-Founder",
+        image: "/assets/about-us/Videep.jpeg",
+    },
 ];
 
 export const defaultConvoHistory = {
-  role: "User",
-  message: "Hello",
+    role: "User",
+    message: "Hello",
 };
