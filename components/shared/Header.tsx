379f8c5c4bf721f11df7430ded5a520ec92cfb33
Adrish7 <adrishmajumder7@gmail.com>
2025-08-26 21:15:57 +0800

    Added Sign Up Button

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,10 +3,10 @@ import React from "react";
 import NavItems from "./NavItems";
 import { MenuIcon } from "lucide-react";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuTrigger,
-    DropdownMenuItem,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
 } from "../ui/dropdown-menu";
 import { navLinks } from "../../constants";
 import Link from "next/link";
@@ -15,39 +15,44 @@ import { UserButton } from "@clerk/nextjs";
 import { SignedOut } from "@clerk/clerk-react";
 
 const Header = () => {
-    return (
-        <div className="flex flex-row mx-24 justify-between h-18 items-center">
-            <div className="flex flex-row gap-1">
-                <img src="/assets/link_green.png" className="w-8 h-8"></img>
-                <h1 className="font-semibold text-lg">The LINK Project</h1>
-            </div>
-            <div className="flex flex-row justify-center items-center gap-10">
-                <NavItems className="hidden md:block" />
-                <div className="hidden md:block"></div>
-                <UserButton />
-                <SignedOut>
-                    <Button asChild>
-                        <Link href="/sign-in">Log in</Link>
-                    </Button>
-                </SignedOut>
-            </div>
-            <div className="relative md:hidden">
-                <DropdownMenu>
-                    <DropdownMenuTrigger>
-                        <MenuIcon />
-                    </DropdownMenuTrigger>
-                    <DropdownMenuContent>
-                        {navLinks.map((navLink) => (
-                            <DropdownMenuItem key={navLink.name} asChild>
-                                <Link href={navLink.route}>{navLink.name}</Link>
-                            </DropdownMenuItem>
-                        ))}
-                        <DropdownMenuItem></DropdownMenuItem>
-                    </DropdownMenuContent>
-                </DropdownMenu>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-row mx-24 justify-between h-18 items-center">
+      <div className="flex flex-row gap-1">
+        <img src="/assets/link_green.png" className="w-8 h-8"></img>
+        <h1 className="font-semibold text-lg">The LINK Project</h1>
+      </div>
+      <div className="flex flex-row justify-center items-center gap-10">
+        <NavItems className="hidden md:block" />
+        <div className="hidden md:block"></div>
+        <UserButton />
+        <SignedOut>
+          <div className="flex gap-2">
+            <Button asChild>
+              <Link href="/sign-up">Sign up</Link>
+            </Button>
+            <Button asChild>
+              <Link href="/sign-in">Log in</Link>
+            </Button>
+          </div>
+        </SignedOut>
+      </div>
+      <div className="relative md:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <MenuIcon />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            {navLinks.map((navLink) => (
+              <DropdownMenuItem key={navLink.name} asChild>
+                <Link href={navLink.route}>{navLink.name}</Link>
+              </DropdownMenuItem>
+            ))}
+            <DropdownMenuItem></DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
