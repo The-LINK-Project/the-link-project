ccf6165db3b02b8900b40b1e6202438a0d793af3
RuhanGupta <gupta56411@gapps.uwcsea.edu.sg>
2025-08-31 16:53:27 +0800

    headeronphone

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -16,41 +16,83 @@ import { SignedOut } from "@clerk/clerk-react";
 
 const Header = () => {
   return (
-    <div className="flex flex-row mx-24 justify-between h-18 items-center">
-      <Link href="/" className="flex flex-row gap-1 items-center">
-        <img src="/assets/link_green.png" className="w-8 h-8" />
-        <h1 className="font-semibold text-lg">The LINK Project</h1>
-      </Link>
-      <div className="flex flex-row justify-center items-center gap-10">
-        <NavItems className="hidden md:block" />
-        <div className="hidden md:block"></div>
-        <UserButton />
-        <SignedOut>
-          <div className="flex gap-2">
-            <Button asChild>
-              <Link href="/sign-up">Sign up</Link>
-            </Button>
-            <Button asChild>
-              <Link href="/sign-in">Sign in</Link>
-            </Button>
-          </div>
-        </SignedOut>
+    <div className="relative h-18">
+      {/* Desktop / Laptop header */}
+      <div className="hidden md:flex flex-row justify-between items-center px-24 h-18">
+        {/* Logo */}
+        <Link href="/" className="flex flex-row gap-1 items-center">
+          <img src="/assets/link_green.png" className="w-8 h-8" />
+          <h1 className="font-semibold text-lg">The LINK Project</h1>
+        </Link>
+
+        {/* Nav + profile */}
+        <div className="flex flex-row justify-center items-center gap-10">
+          <NavItems className="" />
+          <UserButton />
+          <SignedOut>
+            <div className="flex gap-2">
+              <Button asChild>
+                <Link href="/sign-up">Sign up</Link>
+              </Button>
+              <Button asChild>
+                <Link href="/sign-in">Sign in</Link>
+              </Button>
+            </div>
+          </SignedOut>
+        </div>
       </div>
-      <div className="relative md:hidden">
+
+      {/* Mobile header */}
+      <div className="flex md:hidden items-center justify-between px-4 py-4 relative">
+        {/* Logo left */}
+        <Link href="/" className="flex flex-row gap-1 items-center">
+          <img src="/assets/link_green.png" className="w-8 h-8" />
+          <h1 className="font-semibold text-lg truncate">The LINK Project</h1>
+        </Link>
+
+      {/* Hamburger menu right */}
+      <div>
         <DropdownMenu>
           <DropdownMenuTrigger>
             <MenuIcon />
           </DropdownMenuTrigger>
-          <DropdownMenuContent>
+          <DropdownMenuContent className="flex flex-col gap-2">
             {navLinks.map((navLink) => (
               <DropdownMenuItem key={navLink.name} asChild>
                 <Link href={navLink.route}>{navLink.name}</Link>
               </DropdownMenuItem>
             ))}
-            <DropdownMenuItem></DropdownMenuItem>
+
+            {/* Profile icon with text */}
+            <DropdownMenuItem className="flex items-center gap-2">
+              <UserButton
+                appearance={{
+                  elements: {
+                    userButtonAvatarBox: "w-6 h-6", // smaller avatar
+                  },
+                }}
+              />
+              <span className="text-sm font-medium">Profile</span>
+            </DropdownMenuItem>
+
+            <SignedOut>
+              <DropdownMenuItem asChild>
+                <Button asChild>
+                  <Link href="/sign-up">Sign up</Link>
+                </Button>
+              </DropdownMenuItem>
+              <DropdownMenuItem asChild>
+                <Button asChild>
+                  <Link href="/sign-in">Sign in</Link>
+                </Button>
+              </DropdownMenuItem>
+            </SignedOut>
           </DropdownMenuContent>
         </DropdownMenu>
       </div>
+
+      </div>
+
     </div>
   );
 };
