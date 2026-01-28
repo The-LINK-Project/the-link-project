652617d87488d96885fe6233df19bf4a48f1f04e
Adrish7 <adrishmajumder7@gmail.com>
2026-01-28 20:26:02 +0800

    Remove Take a Quiz button

--- components/results/QuizNoResults.tsx
+++ components/results/QuizNoResults.tsx
@@ -1,15 +1,10 @@
 import React from "react";
 import { Trophy } from "lucide-react";
-
 import { useTranslations } from "next-intl";
-
-
-import { Button } from "../ui/button";
-import Link from "next/link";
-
 
 const QuizNoResults = () => {
     const t = useTranslations("quizNoResults");
+
     return (
         <div className="bg-white rounded-xl p-12 shadow-sm border border-gray-200 text-center">
             <div className="p-4 bg-gray-50 rounded-full w-20 h-20 mx-auto mb-4 flex items-center justify-center">
@@ -18,13 +13,9 @@ const QuizNoResults = () => {
             <h3 className="text-xl font-semibold text-gray-900 mb-2">
                 {t("info1")}
             </h3>
-
-            <p className="text-gray-600 mb-6">
+            <p className="text-gray-600">
                 {t("info2")}
             </p>
-            <button className="px-6 py-3 bg-gray-900 text-white rounded-lg hover:bg-gray-800 transition-colors cursor-pointer">
-                {t("info3")}
-            </button>
         </div>
     );
 };
